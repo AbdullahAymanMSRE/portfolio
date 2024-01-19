59df1852dc4b8fdf3c7b0650d02d955a7f56643f
Abdullah Ayman <abdullahaymanmsre@gmail.com>
2024-01-19 13:55:41 +0200

    home page made responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,27 @@
+import "../styles/components/nav.scss";
 import { Link } from "react-router-dom";
 import logoImg from "../assets/logo.png";
 import { Link as ScrollLink } from "react-scroll";
+import { useState } from "react";
 
 export default function Nav({ portfolioScroll }) {
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
     <nav className="nav-component">
       <div className="logo">
         <img src={logoImg} alt="logo" />
         <h1>Abdullah Ayman</h1>
       </div>
-      <ul className="links-list">
+      <div
+        className={"burger" + (navOpen ? " open" : "")}
+        onClick={() => setNavOpen((n) => !n)}
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <ul className={"links-list"}>
         <li>
           <button onClick={portfolioScroll} data-text="Portfolio">
             Portfolio
